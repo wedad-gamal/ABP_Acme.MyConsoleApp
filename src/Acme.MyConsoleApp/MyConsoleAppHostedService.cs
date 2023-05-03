678bb3caff42fe05f28a0b9e8aff7fbfa86060d0
Wedad <wedad.gamal@gmail.com>
2023-05-03 13:02:26 +0300

    Add project files.

---
+++ src/Acme.MyConsoleApp/MyConsoleAppHostedService.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Volo.Abp;
+
+namespace Acme.MyConsoleApp;
+
+public class MyConsoleAppHostedService : IHostedService
+{
+    private readonly IAbpApplicationWithExternalServiceProvider _abpApplication;
+    private readonly HelloWorldService _helloWorldService;
+
+    public MyConsoleAppHostedService(HelloWorldService helloWorldService, IAbpApplicationWithExternalServiceProvider abpApplication)
+    {
+        _helloWorldService = helloWorldService;
+        _abpApplication = abpApplication;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await _helloWorldService.SayHelloAsync();
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await _abpApplication.ShutdownAsync();
+    }
+}
